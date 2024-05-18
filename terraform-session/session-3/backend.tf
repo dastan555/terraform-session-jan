53161159f4315791437a94b5836bc7d86cf2e00c
Dastan Ruslanbekov <dastanruslanbekov@Dastans-Air.hsd1.il.comcast.net>
2024-05-18 14:35:55 -0500

    updated

--- terraform-session/session-3/backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-    backend "s3" {
-        bucket = "terraform-session-jan-das"
-        key = "session-3/terraform.tfstate"
-        region = "us-east-1"
-    }
-}
--- terraform-sessions/session-3/outputs.tf
+++
@@ -1,5 +0,0 @@
-# output "instance_public_ip" {
-#     description = "this prints the public ip of the instance"
-#     value = aws_instance.web.public_ip
-# }
-
